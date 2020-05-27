c80caca6a441b5250c312b4695644b72c7ae3c66
James Massardo <james@dxrf.com>
2020-05-27 16:35:32 +0000

    add additional attributes
    Signed-off-by: James Massardo <james@dxrf.com>

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,1 +1,4 @@
 default['windows_license']['product_key'] = nil
+default['windows_license']['skms_server'] = nil
+default['windows_license']['skms_port'] = 1688
+default['windows_license']['skms_domain'] = nil
